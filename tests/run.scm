b00cc99cbef3405ae0e826a79848381489f50744
David Krentzlin <david.krentzlin@gmail.com>
2014-03-25 13:59:47 +0100

    readded tests

---
+++ tests/run.scm
@@ -0,0 +1,10 @@
+(use test test-generative)
+
+(test-generative ((the-number (lambda () (random 100))))
+   (test-assert "it's numeric"  (number? the-number))
+   (test-assert "it's positive" (positive? the-number))
+   ;(test-assert "it's smaller that 50" (< the-number 50))
+   )
+
+
+(test-exit)
